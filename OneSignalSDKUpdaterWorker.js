ec67ec4f1a89d07d23a9ba2242206c59d9587e44
mohamedlasheenanalysis <mohamedlasheen2019@gmail.com>
2025-10-08 01:35:04 -0700

    Add files via upload

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+// Updater worker for OneSignal. For updates to the service worker.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+// Import the core OneSignal service worker. This file is required for push notifications.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
